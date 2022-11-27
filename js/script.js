34873f0d9a17793bfed7db46207bbc94006704ec
romanjhyltsou <romanjhyltsou@gmail.com>
2022-11-28 01:29:31 +0300

    feat: added fun animation and made ahadow_dog.png

---
+++ js/script.js
@@ -0,0 +1,18 @@
+const canvas = document.getElementById('canvas1');
+const ctx = canvas.getContext('2d');
+const CANVAS_WIDTH = canvas.width = 600;
+const CANVAS_HEIGHT = canvas.height = 600;
+
+const playerImage = new Image();
+playerImage.src = '../assets/img/shadow_dog.png';
+const spriteWidth = 575;  // 6876canvasWidthPng/12column=573
+const spriteHeight = 523; // 5230canvasHeightPng/10row=523
+
+function animate(){
+  ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+  /* ctx.fillRect(50, 50, 100, 100); */
+  //ctx.drawImage(image, sx, sy, sw, sh, dx, dy, dw, dh);
+  ctx.drawImage(playerImage, 0, 0, spriteWidth, spriteHeight, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+  requestAnimationFrame(animate);
+}
+animate();
